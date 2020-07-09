593af4fc16d1ad3c954392e80653cdf4294d5696
Xavi <xavierisachalemany@gmail.com>
2020-07-09 19:41:50 +0200

    finalized desktop version

--- main.js
+++ main.js
@@ -1,21 +1,44 @@
+// DOM variables
 const $shareButton = document.getElementById('shareButton');
 const $shareIcon = $shareButton.childNodes[1];
+const $tooltip = document.getElementById('tooltip');
 const $footer = document.getElementsByClassName('card__footer')[0];
 
+// Assets variables
 const footerShareClass = 'card__footer--share';
 const srcShareIcon = './images/icon-share.svg';
 const srcShareIconWhite = './images/icon-share-white.svg';
 
+// Functions
 const hasClass = (el, wantedClass) => el.classList.contains(wantedClass);
+const calcTooltipPosition = ($button, $tooltip) => {
+  const $arrow = $tooltip.childNodes[3];
+  const refTop = $button.offsetTop;
+  const refLeft = $button.offsetLeft + $button.offsetWidth / 2;
 
-$shareButton.addEventListener('click', () => {
+  const top = refTop - ($tooltip.offsetHeight + $arrow.offsetHeight + 5);
+  const left = refLeft - $tooltip.offsetWidth / 2;
+
+  return [top, left];
+};
+const setTooltipPosition = (button, $tooltip) => {
+  const positions = calcTooltipPosition(button, $tooltip);
+
+  $tooltip.style.top = `${positions[0]}px`;
+  $tooltip.style.left = `${positions[1]}px`;
+};
+
+// Events
+$shareButton.addEventListener('click', function () {
   if (hasClass($footer, footerShareClass)) {
     $footer.classList.remove(footerShareClass);
-    setTimeout(() => {
-      $shareIcon.src = srcShareIcon;
-    }, 500);
+
+    // Change img src button
+    $shareIcon.src = srcShareIcon;
   } else {
     $footer.classList.add(footerShareClass);
+    setTooltipPosition(this, $tooltip);
+    // Change img src button
     $shareIcon.src = srcShareIconWhite;
   }
 });
